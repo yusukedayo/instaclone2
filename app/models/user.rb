c2b66e4c7b3369e574aef632fc89c80aaa071dc4
yusukedayo <89657123+yusukedayo@users.noreply.github.com>
2022-01-02 22:59:30 +0900

    modelの確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   validates :username, uniqueness: true, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, if: -> { new_record? || changes[:email] }
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
