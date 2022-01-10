d233fe2e0a197ed80e8aaa0c1a7f67c1e47262a0
yusukedayo <89657123+yusukedayo@users.noreply.github.com>
2022-01-10 17:47:28 +0900

    コメントの修正

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -22,7 +22,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fit: [1000, 1000]
+  process resize_to_limit: [1000, 1000]
   # process scale: [200, 300]
   #
   # def scale(width, height)
@@ -36,9 +36,9 @@ class PostImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
